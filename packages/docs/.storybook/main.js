63b5698afe5ccc34668247dbae70ce92d7915516
sollandre <andrebmsolla@gmail.com>
2023-06-03 13:51:21 -0300

    fix: storybook downgrade

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,25 +1,26 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.tsx"],
-  addons: [
+module.exports = {
+  "stories": [
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
+  ],
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y"
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
   },
-  docs: {
-    autodocs: true,
-    defaultName: 'Docs'
+  "features": {
+    "storyStoreV7": true
   },
   viteFinal: (config, { configType }) => {
-    if(configType === 'PRODUCTION') {
+    if (configType === 'PRODUCTION') {
       config.base = '/design-system/'
     }
+
     return config
   }
-};
-export default config;
+}
--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,22 +1,14 @@
 import { themes } from '@storybook/theming'
-import type { Preview } from '@storybook/react'
-/** @type { import('@storybook/react').Preview } */
-const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-    docs: {
-      theme: themes.dark
-    },
-    backgrounds: {
-      default: 'dark'
-    }
-  },
-};
 
-export default preview;
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    theme: themes.dark,
+  }
+}
